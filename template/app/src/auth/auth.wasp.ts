4d6feb7617493115db726f08d72249e4568652a4
FranciscoXavierCuevaCarrera <fxc.cueva@yavirac.edu.ec>
2026-09-01 11:22:07 -0500

    fix: switch auth method to usernameAndPassword eliminating all email sender dependencies

--- template/app/src/auth/auth.wasp.ts
+++ template/app/src/auth/auth.wasp.ts
@@ -8,51 +8,19 @@ import {
 
 import { LoginPage } from "./LoginPage" with { type: "ref" };
 import { SignupPage } from "./SignupPage" with { type: "ref" };
-import {
-  getDiscordAuthConfig,
-  getDiscordUserFields,
-  getEmailUserFields,
-  getGitHubAuthConfig,
-  getGitHubUserFields,
-  getGoogleAuthConfig,
-  getGoogleUserFields,
-} from "./userSignupFields" with { type: "ref" };
+import { getUsernameUserFields } from "./userSignupFields" with { type: "ref" };
 
-const emailAuthMethod: NonNullable<AuthMethods["email"]> = {
-  userSignupFields: getEmailUserFields,
-};
-
-// Plug the following authentication methods in the `authConfig` below to enable them.
-// Do note that `email` and `usernameAndPassword` are mutually exclusive.
-// @ts-expect-error Demo purposes
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const usernameAndPasswordAuthMethod: NonNullable<
   AuthMethods["usernameAndPassword"]
-> = {};
-// @ts-expect-error Demo purposes
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const googleAuthMethod: NonNullable<AuthMethods["google"]> = {
-  userSignupFields: getGoogleUserFields,
-  configFn: getGoogleAuthConfig,
-};
-// @ts-expect-error Demo purposes
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const gitGubAuthMethod: NonNullable<AuthMethods["gitHub"]> = {
-  userSignupFields: getGitHubUserFields,
-  configFn: getGitHubAuthConfig,
-};
-// @ts-expect-error Demo purposes
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const discordAuthMethod: NonNullable<AuthMethods["discord"]> = {
-  userSignupFields: getDiscordUserFields,
-  configFn: getDiscordAuthConfig,
+> = {
+  userSignupFields: getUsernameUserFields,
 };
 
 // 🔐 Auth out of the box! https://wasp.sh/docs/auth/overview
 export const authConfig: Auth = {
   userEntity: "User",
   methods: {
-    email: emailAuthMethod,
+    usernameAndPassword: usernameAndPasswordAuthMethod,
   },
   onAuthFailedRedirectTo: "/login",
   onAuthSucceededRedirectTo: "/app",
